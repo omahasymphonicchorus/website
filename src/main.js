dfa57f5fd34d4ea3bd0865798a7795e00573efe2
Nick Meyer <nick@e3b0c442.io>
2019-11-17 17:20:15 -0600

    Install Now UI Kit and fix errors

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import NowUIKit from './plugins/now-ui-kit';
 
 Vue.config.productionTip = false
+
+Vue.use(NowUIKit);
 
 new Vue({
   render: h => h(App),
