66ef74da769e74e464c3c215c36ec670dd044309
Nick Meyer <nick@e3b0c442.io>
2019-11-17 20:05:39 -0600

    Begin navbar, load site name

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader :content="content" />
+    <AppFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./layout/AppHeader.vue";
+import AppFooter from "./layout/AppFooter.vue";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    AppHeader,
+    AppFooter
+  },
+  data: function() {
+    return {
+      content: null
+    };
+  },
+  async mounted() {
+    this.content = await fetch(`${location.origin}/data/index.json`).then(r =>
+      r.json()
+    );
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700,200");
+@import "~@fortawesome/fontawesome-free/scss/fontawesome.scss";
 </style>
---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <Navbar
+      position="fixed"
+      type="primary"
+      :transparent="transparent"
+      :color-on-scroll="colorOnScroll"
+      menu-classes="ml-auto"
+    >
+      <template>
+        <a class="navbar-brand" href="/">{{ content.title }}</a>
+      </template>
+    </Navbar>
+  </header>
+</template>
+
+<script>
+import { Navbar } from "@/components";
+export default {
+  data: function() {
+    return {
+      transparent: true,
+      colorOnScroll: 400
+    };
+  },
+  props: ["content"],
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style></style>
